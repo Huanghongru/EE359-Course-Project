41ff048888e6d577eb89ac6a3755bba3e73abad0
seahunter4 <641375245@qq.com>
2018-06-29 02:14:33 -0700

    Add PCA, LR, LSVM

---
+++ classical.py
@@ -0,0 +1,96 @@
+# classical approach PCA, SVM and Logistic regression
+
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC, SVC
+from sklearn.model_selection import cross_val_score
+from sklearn.externals import joblib
+import time
+from utils import *
+
+def load_data(dir=DATA_PATH):
+    data = np.load(dir+'data.npy')
+    label = np.load(dir+'target.npy')
+    print("Successfully load data and labels.")
+    return data, label
+
+def doPCA(data,n_components=0):
+    """
+    Perform PCA on the input data
+    :param data: the original data
+    :param n_components: param used in sklearn's PCA
+    :return: processed data
+    """
+    start = time.time()
+    print("Starting PCA with n_components={}.".format("default" if n_components==0 else n_components))
+    if n_components == 0:
+        pca_model = PCA()
+    else:
+        pca_model = PCA(n_components=n_components)
+    pca_model.fit(data)
+    print("PCA time cost: {}".format(time.time()-start))
+    joblib.dump(pca_model, PCA_MODEL_PATH + "n_components={}.pkl".format("default" if n_components==0 else n_components))
+    processed_data = pca_model.transform(data)
+    np.save(PCA_DATA_PATH + "n_components={}.npy".format("default" if n_components==0 else n_components), processed_data)
+    print("Variance ratio:",pca_model.explained_variance_ratio_)
+    return processed_data
+
+def LR(data, labels,
+       regularization_coef = 1,
+       penalty = 'l1',
+       verbose = 1):
+    """
+    Perform logistic regression on data
+    :param data: the input data
+    :param labels: the corresponding labels
+    :return:
+    """
+    start = time.time()
+    print("Starting Logistic Regression.")
+    clf = LogisticRegression(penalty=penalty, C=regularization_coef, verbose=verbose)
+    scores = cross_val_score(clf, data, labels, cv=5)
+    print("LR time cost:",time.time()-start)
+    print("CV scores:",scores)
+    return np.average(scores)
+
+def LinearSVM(data, labels,
+              penalty_coef = 1,
+              penalty = 'l1',
+              max_iter = 10000,
+              verbose = 1):
+    start = time.time()
+    print("Starting Linear SVM.")
+    if data.shape[0] > data.shape[1]:
+        clf = LinearSVC(penalty=penalty, C=penalty_coef, max_iter=max_iter, dual=False, verbose=verbose)
+    else:
+        clf = LinearSVC(penalty=penalty, C=penalty_coef, max_iter=max_iter, dual=True, verbose=verbose)
+    scores = cross_val_score(clf, data, labels, cv=5)
+    print("Linear SVM time cost:",time.time()-start)
+    print("CV scores:",scores)
+    return np.average(scores)
+
+if __name__ == "__main__":
+    data, labels = load_data()
+
+    ''' PCA '''
+    # for para in N_COMPONENTS:
+    #     doPCA(data, para)
+
+    ''' LR '''
+    acc = LR(data, labels)
+    print("LR on original data, acc:", acc)
+    for para in N_COMPONENTS:
+        name = "n_components=" + str("default" if para==0 else para)
+        data = np.load(PCA_DATA_PATH + name+'.npy')
+        acc = LR(data, labels)
+        print("LR on PCA({}) data, acc:".format(name), acc)
+
+    ''' SVM '''
+    acc = LinearSVM(data, labels)
+    print("Linear SVM on original data, acc:", acc)
+    for para in N_COMPONENTS:
+        name = "n_components=" + str("default" if para == 0 else para)
+        data = np.load(PCA_DATA_PATH + name + '.npy')
+        acc = LinearSVM(data, labels)
+        print("Linear SVM on PCA({}) data, acc:".format(name), acc)
---
+++ utils.py
@@ -0,0 +1,6 @@
+
+DATA_PATH = "Gene_Chip_Data/"
+PCA_DATA_PATH = "./data/PCA/"
+PCA_MODEL_PATH = "./model/PCA"
+N_COMPONENTS = [0, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
+C = [0.001, 0.01, 0.1, 1., 10., 100., 1000.]
